5fb235c0c1f570b3e51b17ebf3e45210570c00a7
zapatagustin <77631596+zapatagustin@users.noreply.github.com>
2021-12-09 18:18:03 -0300

    Update Footer.js
    tuma update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
                 <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
                 <div className="flex flex-col justify-between lg:flex-row items-center">
-                    <p>Te dejo mis redes, por si no funciona el form!</p>
+                    <p>I leave my networks, in case that the form does not work</p>
                     <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
                         <a
                             href="https://www.linkedin.com/in/agustin-zapata-543779204/"
